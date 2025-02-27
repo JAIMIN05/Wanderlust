ec2ea1202f9f287a88a827200ff975eac37f9412
Jaimin <jaiminkaneriya@gmail.com>
2025-02-27 14:55:10 +0530

    Add vercel.json file

--- app.js
+++ app.js
@@ -101,6 +101,7 @@ app.use((err,req,res,next) => {
     res.status(statusCode).render("error.ejs",{message});
 });
 
-app.listen(8080, ()=>{   
-    console.log("Working");
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
